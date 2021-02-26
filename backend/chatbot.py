35a775ee49f8652451fe80390fae9120ae09ab33
Donald <Donaldchen34@gmail.com>
2021-02-26 17:32:10 -0500

    fixed global variables and added flag functions

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -1,11 +1,11 @@
 import spacy
-from environment import shapes, colors, addShape,delShape,findShape,moveShape,holdShape, GRID_SIZE, Grid,showGrid
+from environment import SHAPES, COLORS, addShape,delShape,findShape,moveShape,holdShape, GRID_SIZE, GRID,showGrid
 
 #Create function for conjunctions(and) to make sentences easier to read
 
 nlp = spacy.load('en_core_web_sm')
 
-actions = {'find':findShape,'delete':delShape,'add':addShape,'move':moveShape,'pick':holdShape}
+ACTIONS = {'FIND':findShape, 'DELETE':delShape, 'ADD':addShape, 'MOVE':moveShape, 'PICK':holdShape}
 
 #@Param sentence- string
 #Sentence - Takes the format "[action] [adj] [noun] [x] [y]"
@@ -19,11 +19,11 @@ def readSentence(sentence):
     row,col = -1,-1,
     for token in processed_sentence:
         if token.pos_ == 'VERB':
-            action = token.text.lower()
+            action = token.text.upper()
         if token.pos_ == 'ADJ':
-            color = token.text.lower()
+            color = token.text.upper()
         if token.pos_ == 'NOUN':
-            shape = token.text.lower()
+            shape = token.text.upper()
         if token.pos_ == 'NUM':
             if row == -1:
                 row = int(token.text)
@@ -35,35 +35,32 @@ def readSentence(sentence):
 
     return(shape,color,action,row,col)
 
+def checkShape(noun):
+    return noun in SHAPES
 
-#@Param - noun,adj,verb are strings, row,col are ints
-#Checks to see if the semantics are in our lists:
-#check[noun,adjective,verb,location]
-#0 -> no error, 1 -> error
-def checkSemantics(noun,adj,verb,row,col):
-    check = [0,0,0,0]
-    if noun not in shapes:
-        check[0] = 1
-    if adj not in colors:
-        check[1] = 1
-    if verb not in actions:
-        check[2] = 1
-    if row > GRID_SIZE or col > GRID_SIZE \
-        or row < 0 or col < 0:
-        check[3] = 1
+def checkColor(adj):
+    return adj in COLORS
 
-    return check
+def checkAction(action):
+    return action in ACTIONS
+
+def checkLocation(row,col):
+    return row > GRID_SIZE or col > GRID_SIZE \
+        or row < 0 or col < 0
+
+def emptyPosition(row,col):
+    return GRID[row][col] == ("", "", 0)
 
 #@Param flags- the check from @func checkSemantics
 #Returns the reponse_number based on what conditions are met
-def doAction(action,shape,color,row,col,flags):
+def doAction(action,shape,color,row,col):
 
     if action == 'add':
-        if flags[1] == 1:
+        if not checkColor(color):
             return 4
-        elif flags[3] == 1:
+        elif not checkLocation(row,col):
             return 10
-        elif Grid[row][col] == (0,0,0):
+        elif emptyPosition(row,col):
             addShape(shape=shape,color = color,row = row,col=col)
             return 0
         else:
@@ -71,7 +68,7 @@ def doAction(action,shape,color,row,col,flags):
 
     foundShape = findShape(shape=shape,color=color)
     if len(foundShape) > 1:
-        if color in colors:
+        if color in COLORS:
             return 5  # Change to ask for location
         else:
             return 4
@@ -79,7 +76,7 @@ def doAction(action,shape,color,row,col,flags):
         return 6
     else:
         if action == 'move':
-            if flags[3] == 1:
+            if not checkLocation(row,col) == 1:
                 return 7
             else:
                 moveShape(x1= foundShape[0][0],y1=foundShape[0][1],x2=row,y2=col)
@@ -87,7 +84,7 @@ def doAction(action,shape,color,row,col,flags):
             return 8
         if action == 'delete':
             if row != -1 and col != -1: #Case: Coordinates are given
-                if Grid[row][col] == (shapes[shape],colors[color],0):
+                if GRID[row][col] == (SHAPES[shape], COLORS[color], 0):
                     delShape(row,col)
                 else:
                     return 6
@@ -118,24 +115,15 @@ def response(response_number):
 #Takes string parameter and outputs response based on the string
 def chatbot(sentence):
     shape, color, action, row, col = readSentence(sentence)
-    flags = checkSemantics(shape,color,action,row,col)
 
     response_number = -1
 
-    showGrid()
-
-    if flags[0] == 1: # Check if shape is valid
+    if not checkShape(shape) == 1: # Check if shape is valid
         response_number = 2
-    elif flags[2] == 1: #Check if action is valid
+    elif not checkAction(action) == 1: #Check if action is valid
         response_number = 3
     else:
-        response_number = doAction(action=action,shape=shape,color=color,row=row,col=col,flags=flags)
-
-    print('-----------------------------------')
-    showGrid()
-    print(response(response_number))
-    print('-----------------------------------')
-
+        response_number = doAction(action=action,shape=shape,color=color,row=row,col=col)
 
     return(response(response_number))
 
--- backend/environment.py
+++ backend/environment.py
@@ -1,10 +1,10 @@
 GRID_SIZE = 4
 #Tuple is (shape, color, height)
-Grid = [[(0,0,0) for i in range(GRID_SIZE)] for j in range(GRID_SIZE)]
+GRID = [[("", "", 0) for i in range(GRID_SIZE)] for j in range(GRID_SIZE)]
 
-shapes = {"cube": 1, "pyramid": 2, "box": 3}
-colors = {"red": 1, "blue": 2, "green": 3}
-claw_pos = GRID_SIZE // 2, GRID_SIZE // 2
+SHAPES = set(["CUBE", "PYRAMID", "BOX"])
+COLORS = set(["RED", "BLUE", "GREEN"])
+CLAW_POS = GRID_SIZE // 2, GRID_SIZE // 2
 
 #Change height parameter in functions to a lambda to check for comparison words:
 #ie. Find a block that is TALLER than this pyramid
@@ -13,14 +13,14 @@ claw_pos = GRID_SIZE // 2, GRID_SIZE // 2
 def findShape(shape, color = None,height = 0):
     foundShapes = []
 
-    for y, row in enumerate(Grid):
+    for y, row in enumerate(GRID):
         for x, pos in enumerate(row):
             found = True
-            if shapes[shape] != pos[0]:
+            if SHAPES[shape] != pos[0]:
                 found = False
             if color:
-                if color in colors:
-                    if colors[color] != pos[1]:
+                if color in COLORS:
+                    if COLORS[color] != pos[1]:
                         found = False
                 else:
                     found = False
@@ -34,32 +34,32 @@ def findShape(shape, color = None,height = 0):
     return foundShapes
 
 def addShape(row,col, shape, color, height = 0):
-    global Grid
+    global GRID
 
-    if shape in shapes and color in colors:
-        Grid[row][col] = (shapes[shape],colors[color],height)
+    if shape in SHAPES and color in COLORS:
+        GRID[row][col] = (SHAPES[shape], COLORS[color], height)
 
 def delShape(row,col):
-    global Grid
+    global GRID
 
-    Grid[row][col] = (0,0,0)
+    GRID[row][col] = (0, 0, 0)
 
 
 #x1,y1 is moving from
 #x2,y2 is moving to
 def moveShape(x1,y1,x2,y2):
-    global  Grid
+    global  GRID
 
-    Grid[x2][y2] = Grid[x1][y1]
+    GRID[x2][y2] = GRID[x1][y1]
     delShape(x1, y1)
 
 
 def holdShape(row,col):
-    global claw_pos
-    claw_pos = row,col
+    global CLAW_POS
+    CLAW_POS = row, col
 
 
 def showGrid():
-    for i in Grid:
+    for i in GRID:
         print(i)
 
