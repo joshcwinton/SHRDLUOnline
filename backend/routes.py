bfd869be77018cece53ebf41d4b6e645ac8b2aeb
svhnstorage <svhnstorage@gmail.com>
2021-02-25 22:29:43 -0500

    Took out unused from routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -15,12 +15,6 @@ def main_route():
 @app.route('/sentences', methods = ['GET'])
 def return_All():
     return jsonify({'sentences' : sentences})
-
-#endpoint GET SPECIFIC (UNUSED)
-@app.route('/sentences/<string:sass>', methods = ['GET'])
-def return_EndPoint(sass):
-    sens = [sentences for sentences in sentences if sentences['sass'] == sass]
-    return jsonify({'sentences' : sens[0]})
             
 #endpoint POST
 @app.route('/sentences', methods = ['GET','POST'])
@@ -32,8 +26,6 @@ def test_route():
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
-
-#doesnt run with this idk why
-#    app.run(host = '0.0.0.0', port=os.getenv('PORT', 5555), debug=True)
+#    app.run(debug = True)
+    app.run(host = '0.0.0.0', port=os.getenv('PORT', 5555), debug=True)
 
