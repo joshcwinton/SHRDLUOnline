9246b293757900316a95bbe5fc01aeaf693e2b94
joshcwinton <joshcwinton@gmail.com>
2021-02-28 00:43:48 -0500

    import Nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 class SHRDLUNavbar extends Component {
   render() {
