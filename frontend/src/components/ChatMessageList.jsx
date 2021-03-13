e403a4d788807d0888021f1bb79a0ef3c16ce2d9
Jasper Cheung <23638735+JasperCheung@users.noreply.github.com>
2021-03-12 22:32:35 -0500

    Scrollable Chat :fire: :fire: :fire:

--- frontend/src/components/ChatMessageList.jsx
+++ frontend/src/components/ChatMessageList.jsx
@@ -4,17 +4,32 @@ import ChatMessage from './ChatMessage';
 
 class ChatMessageList extends Component {
 
-   constructor(props) {
-    super(props);
+  chatContainer = React.createRef();
 
+   constructor(props) {
+     super(props);
+   }
+
+  componentDidUpdate  = () => {
+    this.scrollToMyRef();
   }
+  scrollToMyRef = () => {
+    const scroll = this.chatContainer.current.scrollHeight - this.chatContainer.current.clientHeight;
+
+    this.chatContainer.current.scrollTo(0, scroll);
+  };
   render() {
-    return this.props.messages.map((message, index) =>
+    return (
+      <div ref={this.chatContainer} className="Chat">
+      {this.props.messages.map((message, index) =>(
           <ChatMessage
             key={index}
             message={message.text}
             name={message.name}
-          />);
+          />))}
+      </div>
+
+    );
   }
 }
 
