a729d372be2f7959e30103c5d50260f7f652b81c
Sarvesh <75312440+sarvesh059@users.noreply.github.com>
2021-03-14 10:51:27 +0530

    Update environment.py
    Created the Environment class and added all the functions as methods of Environment class. and added documentation and error handing methods.

--- backend/environment.py
+++ backend/environment.py
@@ -1,70 +1,110 @@
-GRID_SIZE = 4
-#Tuple is (shape, color, height)
-GRID = [[("", "", 0) for i in range(GRID_SIZE)] for j in range(GRID_SIZE)]
+class Environment:
+    """Environment class for shape operations
+    ...
+    Attributes
+    ----------
+    GRID_SIZE : int
+        The size of GRID, predefined 4.
+    SHAPES : list(str)
+        The valid list of shapes that can be inserted in GRID.
+    COLORS : list(str)
+        The valid list of colors that can be added to GRID.
+    CLAW_POS: (int,int)
+        Used to hold the position of a shape. By default initialises at middle of grid.
+    GRID : list(list(tuple(str,str,int)))
+        A 2D array of tuples containing shape, color, height.
 
-SHAPES = set(["CUBE", "PYRAMID", "BOX"])
-COLORS = set(["RED", "BLUE", "GREEN"])
-CLAW_POS = GRID_SIZE // 2, GRID_SIZE // 2
+    Methods
+    -------
+    findShape(shape,color=None,height=0)
+        Returns a list of all matching shapes present in GRID.
+    addShape(row,column,shape,color,height)
+        Add a shape to specified (row,column) of GRID.
+    delShape(row,column)
+        Delete the shape from specified (row,column) of GRID.
+    moveShape(x1,y1,x2,y2)
+        Moves the shape from (x1,y1) to (x2,y2).
+    holdShape(row,col)
+        Hold the shape at specified (row,column).
+    clearBoard
+        Clear the Grid
+    showGrid
+        Print the grid"""
 
-#Change height parameter in functions to a lambda to check for comparison words:
-#ie. Find a block that is TALLER than this pyramid
+    def __init__(self):
+        self.GRID_SIZE = 4
+        self.SHAPES = ["CUBE", "PYRAMID", "BOX"]
+        self.COLORS = ["RED", "BLUE", "GREEN"]
+        self.CLAW_POS = (self.GRID_SIZE//2, self.GRID_SIZE//2)
+        self.GRID = [[("", "", 0) for i in range(self.GRID_SIZE)] for j in range(self.GRID_SIZE)]  # Tuple is (shape, color, height)
 
-#returns all shapes found based on given parameters
-def findShape(shape, color = None,height = 0):
-    foundShapes = []
+    def findShape(self,shape, color=None, height=0):
+        """Returns all shapes found based on given parameters"""
+        foundShapes = []
 
-    for y, row in enumerate(GRID):
-        for x, pos in enumerate(row):
-            found = True
-            if shape != pos[0]:
-                found = False
-            if color:
-                if color in COLORS:
-                    if color != pos[1]:
+        for y, row in enumerate(self.GRID):
+            for x, pos in enumerate(row):
+                found = True
+                if shape != pos[0]:
+                    found = False
+                    continue
+                if color:
+                    if color in self.COLORS:
+                        if color != pos[1]:
+                            found = False
+                            continue
+                    else:
                         found = False
-                else:
-                    found = False
-            if height:
-                if height != pos[2]:
-                    found = False
+                        continue
+                if height:
+                    if height != pos[2]:
+                        found = False
 
-            if found:
-                foundShapes.append((y, x))
+                if found:
+                    foundShapes.append((y, x))
 
-    return foundShapes
+        return foundShapes
 
-def addShape(row,col, shape, color, height = 0):
-    global GRID
+    def addShape(self,row: int,col: int, shape: str, color: str, height = 0) -> None:
+        """Add a shape to the grid at specified row and column
+        """
+        if shape in self.SHAPES and color in self.COLORS:
+            self.GRID[row][col] = (shape, color, height)
 
-    if shape in SHAPES and color in COLORS:
-        GRID[row][col] = (shape, color, height)
-
-def delShape(row,col):
-    global GRID
-
-    GRID[row][col] = ("", "", 0)
-
-
-#x1,y1 is moving from
-#x2,y2 is moving to
-def moveShape(x1,y1,x2,y2):
-    global  GRID
-
-    GRID[x2][y2] = GRID[x1][y1]
-    delShape(x1, y1)
+    def delShape(self,row: int,col: int)-> None:
+        """Deletes the shape at (row,col) position of grid.
+        :raise ValueError if row and col does not lie within grid."""
+        try:
+            self.GRID[row][col] = ("", "", 0)
+        except:
+            raise ValueError('Enter values of within range(0,%d)'%self.GRID_SIZE)
 
 
-def holdShape(row,col):
-    global CLAW_POS
-    CLAW_POS = row, col
+    #x1,y1 is moving from
+    #x2,y2 is moving to
+    def moveShape(self,x1: int,y1: int,x2: int,y2: int)-> None:
+        """Move the shape from (x1,y1) -> (x2,y2)
+        :raise ValueError if row and col does not lie within grid."""
+        try:
+            self.GRID[x2][y2] = self.GRID[x1][y1]
+            self.delShape(x1, y1)
+        except:
+            raise ValueError('Enter values of within range(0,%d)'%self.GRID_SIZE)
 
-def clearBoard():
-    global GRID
-    for x in range(GRID_SIZE):
-        for y in range(GRID_SIZE):
-            GRID[x][y] = ("","",0)
 
-def showGrid():
-    for i in GRID:
-        print(i)
+    def holdShape(self,row: int,col: int)-> None:
+        """Hold the shape at (row,col) in self.CLAW_POS.
+        :raise ValueError if row and col does not lie within grid."""
+        try:
+            self.CLAW_POS = row, col
+        except:
+            raise ValueError('Enter values of within range(0,%d)'%self.GRID_SIZE)
 
+    def clearBoard(self)-> None:
+        """Clears the GRID"""
+        # instead of using nested for loops initialing the grid as empty
+        self.GRID = [[("", "", 0) for i in range(self.GRID_SIZE)] for j in range(self.GRID_SIZE)]  # Tuple is (shape, color, height)
+
+    def showGrid(self)-> None:
+        """Print the GRID."""
+        print(*self.GRID,sep='\n')
