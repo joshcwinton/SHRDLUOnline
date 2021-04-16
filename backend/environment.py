2398d0a56d234d5334b1cb9a9778cb628a09a268
joshcwinton <joshcwinton@users.noreply.github.com>
2021-04-16 00:25:12 +0000

    autopep8 action fixes

--- backend/environment.py
+++ backend/environment.py
@@ -1,4 +1,4 @@
-from render import renderEnvironment 
+from render import renderEnvironment
 
 GRID_SIZE = 4
 #Tuple is (shape, color, height)
@@ -8,11 +8,13 @@ SHAPES = set(["CUBE", "PYRAMID", "SPHERE"])
 COLORS = set(["RED", "BLUE", "GREEN"])
 CLAW_POS = GRID_SIZE // 2, GRID_SIZE // 2
 
-#Change height parameter in functions to a lambda to check for comparison words:
-#ie. Find a block that is TALLER than this pyramid
+# Change height parameter in functions to a lambda to check for comparison words:
+# ie. Find a block that is TALLER than this pyramid
 
-#returns all shapes found based on given parameters
-def findShape(shape, color = None,height = 0):
+# returns all shapes found based on given parameters
+
+
+def findShape(shape, color=None, height=0):
     foundShapes = []
 
     for y, row in enumerate(GRID):
@@ -35,27 +37,29 @@ def findShape(shape, color = None,height = 0):
 
     return foundShapes
 
-def addShape(row,col, shape, color, height = 0):
+
+def addShape(row, col, shape, color, height=0):
     global GRID
 
     if shape in SHAPES and color in COLORS:
         GRID[row][col] = (shape, color, height)
 
     renderEnvironment(GRID)
-    
 
 
-def delShape(row,col):
+def delShape(row, col):
     global GRID
 
     GRID[row][col] = ("", "", 0)
 
     renderEnvironment(GRID)
 
-#x1,y1 is moving from
-#x2,y2 is moving to
-def moveShape(x1,y1,x2,y2):
-    global  GRID
+# x1,y1 is moving from
+# x2,y2 is moving to
+
+
+def moveShape(x1, y1, x2, y2):
+    global GRID
 
     GRID[x2][y2] = GRID[x1][y1]
 
@@ -64,23 +68,26 @@ def moveShape(x1,y1,x2,y2):
     renderEnvironment(GRID)
 
 
-def holdShape(row,col):
+def holdShape(row, col):
     global CLAW_POS
     CLAW_POS = row, col
+
 
 def clearBoard():
     global GRID
     for x in range(GRID_SIZE):
         for y in range(GRID_SIZE):
-            GRID[x][y] = ("","",0)
-    
+            GRID[x][y] = ("", "", 0)
+
     renderEnvironment(GRID)
+
 
 def showGrid():
     for i in GRID:
         print(i)
 
     renderEnvironment(GRID)
+
 
 def getEnvironment():
     return(GRID)
