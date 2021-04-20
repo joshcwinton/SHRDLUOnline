e9f7ca0bc677c855053ded34c667fd3b9223bf1a
svhnstorage <svhnstorage@users.noreply.github.com>
2021-04-20 19:38:36 +0000

    Adjusted files for PEP-8 compliance

--- backend/dbqueries.py
+++ backend/dbqueries.py
@@ -1,53 +1,54 @@
 import pyrebase
 
 config = {
-  "apiKey": "AIzaSyB7EsIaeyJ3MnYF_1fjyzNcYFRLNRtx3x0",
-  "authDomain": "shrdlu-tdb.firebaseapp.com",
-  "databaseURL": "https://shrdlu-tdb-default-rtdb.firebaseio.com",
-  "projectId": "shrdlu-tdb",
-  "storageBucket": "shrdlu-tdb.appspot.com",
-  "messagingSenderId": "420253619610",
-  "appId": "1:420253619610:web:c359b6239511f757a6d623",
-  "measurementId": "G-YX5PBR5SBQ"
-};
+    "apiKey": "AIzaSyB7EsIaeyJ3MnYF_1fjyzNcYFRLNRtx3x0",
+    "authDomain": "shrdlu-tdb.firebaseapp.com",
+    "databaseURL": "https://shrdlu-tdb-default-rtdb.firebaseio.com",
+    "projectId": "shrdlu-tdb",
+    "storageBucket": "shrdlu-tdb.appspot.com",
+    "messagingSenderId": "420253619610",
+    "appId": "1:420253619610:web:c359b6239511f757a6d623",
+    "measurementId": "G-YX5PBR5SBQ"
+}
 
 firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
 
-#stores users message
+# stores users message
+
+
 def archiveSelfMessage(toBeStored):
-	db.child("users").child("user1").push({"Me" : toBeStored})
+    db.child("users").child("user1").push({"Me": toBeStored})
 
-#stores bot message
+# stores bot message
+
+
 def archiveBotMessage(toBeStored):
-	db.child("users").child("user1").push({"SHRDLU" : toBeStored})
+    db.child("users").child("user1").push({"SHRDLU": toBeStored})
 
-#retrieves all messages
+# retrieves all messages
+
+
 def retrieveAllMessages():
-	print (db.child("users").child("user1").get().val())
+    print (db.child("users").child("user1").get().val())
 
-#TODO
-#store environment
-#retireve environment
-#store messages more efficiently
+# TODO
+# store environment
+# retireve environment
+# store messages more efficiently
 
 
-#CRUD with firebase 
-#put data to db
+# CRUD with firebase
+# put data to db
 #db.child("names").push({"name": "raunak"})
 
-#update needs to be fitted with json
-#db.child("names").child("MYjrJEpvUvoNBufufHw").update({"name":"updoot"})
+# update needs to be fitted with json
+# db.child("names").child("MYjrJEpvUvoNBufufHw").update({"name":"updoot"})
 
-#get data
+# get data
 #users = db.child("names").child("MYjrJEpvUvoNBufufHw").get()
-#print(users.key())
+# print(users.key())
 
-#remove data
-#db.child("names").child("MYjrJEpvUvoNBufufHw").remove()
-
-	
-
-
-	
+# remove data
+# db.child("names").child("MYjrJEpvUvoNBufufHw").remove()
