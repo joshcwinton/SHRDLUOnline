17b363fe4d769ad6994832a82dd3d4e26bc23123
joshcwinton <joshcwinton@gmail.com>
2021-05-11 17:13:22 -0400

    new instance page

--- frontend/src/components/CreateInstance.jsx
+++ frontend/src/components/CreateInstance.jsx
@@ -1,23 +1,90 @@
 import React, { Component } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button, Container, Jumbotron } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 class CreateInstance extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instanceName: "",
+      creatorName: "",
+      instanceSize: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit = () => {
+    axios
+      .post("/new_instance", {
+        instanceName: this.state.instanceName,
+        creatorName: this.state.creatorName,
+        instanceSize: this.state.instanceSize,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    this.props.history.push(`/instance/${this.state.instanceName}`);
+  };
+
   render = () => {
     return (
-      <Form>
-        <Form.Group>
-          <Form.Label>Instance name</Form.Label>
-          <Form.Control placeholder="Enter instance name" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Creator</Form.Label>
-          <Form.Control placeholder="Enter your name" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Instance size</Form.Label>
-          <Form.Control placeholder="" />
-        </Form.Group>
-      </Form>
+      <Container>
+        <h1>Create a new instance</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formInstanceName">
+            <Form.Label>Instance name</Form.Label>
+            <Form.Control
+              name="instanceName"
+              type="text"
+              value={this.state.instanceName}
+              placeholder="Enter instance name"
+              onChange={this.handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formCreatorName">
+            <Form.Label>Creator</Form.Label>
+            <Form.Control
+              name="creatorName"
+              type="text"
+              value={this.state.creatorName}
+              placeholder="Enter your name"
+              onChange={this.handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formInstanceSize">
+            <Form.Label>Instance size</Form.Label>
+            <Form.Control
+              name="instanceSize"
+              type="number"
+              value={this.instanceSize}
+              placeholder="Enter instance size, e.g. 5 gives 5x5 square"
+              onChange={this.handleInputChange}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     );
   };
 }
